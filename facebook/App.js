6348798555e933f35d1759896c53268801502001
Kaiofprates <kaioprates@outlook.com>
2020-10-15 19:54:57 -0300

    add face

---
+++ facebook/App.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ActivityIndicator } from 'react-native';
+import * as Facebook from 'expo-facebook';
+
+console.disableYellowBox = true;
+
+export default function App() {
+
+  const [isLoggedin, setLoggedinStatus] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [isImageLoading, setImageLoadStatus] = useState(false);
+
+  facebookLogIn = async () => {
+     try {
+    await Facebook.initializeAsync({
+      appId: '345898209435012',
+    });
+    const {
+      type,
+      token,
+      expirationDate,
+      permissions,
+      declinedPermissions,
+    } = await Facebook.logInWithReadPermissionsAsync({
+      permissions: ['public_profile'],
+    });
+    if (type === 'success') {
+      
+      console.log(`
+      token: ${token}
+      permissions: ${permissions} 
+      `)
+       
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+      alert('Logged in!', `Hi ${(await response.json()).name}!`);
+    } else {
+      // type === 'cancel'
+    }
+  } catch ({ message }) {
+    alert(`Facebook Login Error: ${message}`);
+  }
+  }
+
+  logout = () => {
+    setLoggedinStatus(false);
+    setUserData(null);
+    setImageLoadStatus(false);
+  }
+
+  return (
+    isLoggedin ?
+      userData ?
+        <View style={styles.container}>
+          <Image
+            style={{ width: 200, height: 200, borderRadius: 50 }}
+            source={{ uri: userData.picture.data.url }}
+            onLoadEnd={() => setImageLoadStatus(true)} />
+          <ActivityIndicator size="large" color="#0000ff" animating={!isImageLoading} style={{ position: "absolute" }} />
+          <Text style={{ fontSize: 22, marginVertical: 10 }}>Hi {userData.name}!</Text>
+          <TouchableOpacity style={styles.logoutBtn} onPress={this.logout}>
+            <Text style={{ color: "#fff" }}>Logout</Text>
+          </TouchableOpacity>
+        </View> :
+        null
+      :
+      <View style={styles.container}>
+        <Image
+          style={{ width: 200, height: 200, borderRadius: 50, marginVertical: 20 }}
+          source={require("./assets/snack-icon.png")} />
+        <TouchableOpacity style={styles.loginBtn} onPress={this.facebookLogIn}>
+          <Text style={{ color: "#fff" }}>Login with Facebook</Text>
+        </TouchableOpacity>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e9ebee',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginBtn: {
+    backgroundColor: '#4267b2',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20
+  },
+  logoutBtn: {
+    backgroundColor: 'grey',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    position: "absolute",
+    bottom: 0
+  },
+});
